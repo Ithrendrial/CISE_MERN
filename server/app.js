b7873f1078d63603a19bddf3a5e0efedeeddbe3b
Ithrendrial <rcox2990@gmail.com>
2023-08-18 17:17:17 +1200

    fixing for vercel

--- server/app.js
+++ server/app.js
@@ -11,7 +11,20 @@ const app = express();
 connectDB();
 
 // cors
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors(
+    {
+        origin: ["https://cise-mern-frontend-theta.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }));
+
+// adding
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//     next();
+// })
 
 // Init Middleware
 app.use(express.json({ extended: false }));
